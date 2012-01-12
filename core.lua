0ea6bdb8ba8897f2fa2edbde9ed1c168ad6fb570
yaroot <yaroot@gmail.com>
2012-01-12 15:49:38 +0800

    .

--- core.lua
+++ core.lua
@@ -19,7 +19,7 @@ local config = {
 if(string.upper(UnitName'player') == '\66\65\83\72' and GetRealmName() == '\229\175\146\229\134\176\231\154\135\229\134\160')
     or (string.upper(UnitName'player') == '\89\97\114\111\111\116' and GetRealmName() == '\228\188\138\231\145\159\230\139\137')
     then
-    config.restocklist[58257] = 40 -- 高地泉水
+    config.restocklist[58257] = 60 -- 高地泉水
     config.wontsell[6196] = true -- Noboru's Cudgel
     config.wontsell[3300] = true -- Rabbit's Foot
 end
@@ -29,23 +29,6 @@ local eventFrame = CreateFrame'Frame'
 local addon = {}
 addon.config = config
 addon.eventFrame = eventFrame
-
-local function formatMoney(value)
-    local str = ''
-    if value >= 10000 then
-        str = str .. format('|c00ffd700%dg|r', floor(value / 10000))
-    end
-    if value >= 100 then
-        str = str .. format('|c00c7c7cf%ds|r', (floor(value / 100) % 100))
-    end
-
-    local cooper = floor(value)%100
-    if(cooper>0) then
-        str = str .. format('|c00eda55f%dc|r', cooper)
-    end
-
-    return str
-end
 
 local GetItemID = function(link)
     return tonumber(link:match'item:(%d+):')
@@ -64,29 +47,12 @@ local BuyItem = function(itemid, tobuy)
     end
 
     if(not index) then return end
-
     local name, texture, price, quantity, numAvailable, isUsable, extendedCost = GetMerchantItemInfo(index)
-    local stacksize = GetMerchantItemMaxStack(index)
-
-    if(numAvailable > 0) then
-        tobuy = min(tobuy, numAvailable)
-    end
-    if(tobuy<1) then
-        return
+    if(numAvailable ~= -1 and tobuy < numAvailable) then
+        tobuy = numAvailable
     end
 
-    local loops = floor(tobuy / stacksize)
-    local leftover = tobuy % stacksize
-
-    if(loops > 0) then
-        for i = 1, loops do
-            BuyMerchantItem(index, stacksize)
-        end
-    end
-
-    if(leftover > 0) then
-        BuyMerchantItem(index, leftover)
-    end
+    BuyMerchantItem(index, tobuy)
 end
 
 local Sell = function()
@@ -118,13 +84,6 @@ local Repair = function()
     end
     local gbused = cost - GetRepairAllCost()
     RepairAllItems()
-
-    if(gbused>0) then
-        print('Repair cost guildbank '.. formatMoney(gbused))
-    end
-    if(gbused ~= cost) then
-        print('Repair cost ' .. formatMoney(cost-gbused))
-    end
 end
 
 local Restock = function()
@@ -135,7 +94,9 @@ local Restock = function()
         if(holdsize) then
             local has = GetItemCount(itemID)
             local tobuy = holdsize - has
-            BuyItem(itemID, tobuy)
+            if(tobuy > 0) then
+                BuyItem(itemID, tobuy)
+            end
         end
     end
 end
